384ef753ff9304bf9d6ce89ea885189aff63b0dc
gresa13 <106274248+gresa13@users.noreply.github.com>
2023-12-09 17:25:23 +0100

    Delete Homework 2/Technical Prototype/homework2/homework2_backend directory

--- Homework 2/Technical Prototype/homework2/homework2_backend/src/main/java/com/example/homework2/exceptions/WineryAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.homework2.exceptions;
-
-public class WineryAlreadyExistsException extends RuntimeException{
-    public WineryAlreadyExistsException() {
-        super("Winery already exists");
-    }
-}
